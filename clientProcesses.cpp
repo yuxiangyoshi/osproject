33384023fc9885a873861d3655a8831b87f5847b
Senthil Kumar <Senthil Kumar>
2017-11-17 09:40:38 -0800

    Code cleanup

--- clientProcesses.cpp
+++ clientProcesses.cpp
@@ -1,37 +1,24 @@
-#include <string>
-#include <iostream>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include "Timer.h"
-
+#include "Commons.h"
 using namespace std;
-#define N_PROCESS 10
-
-void *executeAdd(int num){
-    cout << "Number:" << num << endl;
-}
-void *process_work(int num)
-{
-    executeAdd(num);
-}
 
 int main (){
  pid_t pid;
  int i;
  Timer time;
+ Commons c;
  double eTime;
  time.start();   
  cout << "START: Parent process PID = " << getpid() << endl;
 
- for (i = 1; i <= N_PROCESS; i++)
+ for (i = 1; i <= c.COUNT; i++)
   switch ( pid = fork() ) {
   case -1:
     cout << "Error in fork";
 
   case 0:
     cout << "Child " <<i <<" PID = " << getpid() << endl;
-    process_work(i*10000);
+    c.task("localhost", 8888);
     time.elapsedUserTime(eTime);
     cout << "Child " <<i <<" Elapsed User Time: " << eTime << endl;
     return 0;
--- clientThreads.cpp
+++ clientThreads.cpp
@@ -11,72 +11,10 @@
 #include <iostream>
 #include <string.h>
 #include "Timer.h"
+#include "Commons.h"
 using namespace std;
 
 #define N_THREADS 10
-#define BUFSIZ 56
-
-void error(char *msg)
-{
-    perror(msg);
-    exit(-1);
-}
-
-sockaddr_in make_server_addr(char *host, u_short port)
-{
-    sockaddr_in addr;
-    bzero(&addr, sizeof addr);
-    hostent *hp = gethostbyname(host);
-    if (hp == 0)
-    {
-        herror(host);
-        exit(-1);
-    }
-    addr.sin_family = AF_INET;
-    bcopy(hp->h_addr, &addr.sin_addr, hp->h_length);
-    addr.sin_port = htons(port);
-    return addr;
-}
-
-int connect_socket(char *host, u_short port)
-{
-    int status;
-    int tries = 3;
-    sockaddr_in addr = make_server_addr(host, port);
-    int s = socket(AF_INET, SOCK_STREAM, 0);
-    if (s == -1)
-        error("socket()");
-    status = connect(s, (sockaddr*)&addr, sizeof addr);
-    if (status < 0)
-        error("connect refused");
-    return s;
-}
-
-void requestNumFromServer(int server_socket)
-{
-/*
-    int len = strlen(file);
-    int n = write(server_socket, file, len);
-    if (n != len)
-        error("short write");
-*/
-}
-
-void readNumFromServer(int server_socket)
-{
-/*
-    char buf[BUFSIZ];
-    int n;
-    mode_t mode = 0666;
-    int ofd = open(file, O_WRONLY | O_CREAT, mode);
-    if (ofd == -1)
-        perror("open()");
-    while ((n = read(server_socket, buf, BUFSIZ)) > 0)
-        write(ofd, buf, n);
-    close(ofd);
-*/
-}
-
 
 struct Thread_data
 {
@@ -84,21 +22,13 @@ struct Thread_data
     pthread_t thread_id;
     char * host;
     u_short port;
-    char path[BUFSIZ];
 };
-
-int executeAdd(Thread_data & data)
-{
-    int server_socket = connect_socket(data.host, data.port);
-    requestNumFromServer(server_socket);
-    readNumFromServer(server_socket);
-    close(server_socket);
-}
 
 void *thread_work(void *arg)
 {
     Thread_data data = *(Thread_data*)arg;
-    executeAdd(data);
+    Commons c;
+    c.task(data.host, data.port);
     pthread_exit(0);
 }
 
@@ -109,10 +39,12 @@ int main(int argc, char *argv[])
         cerr << "Usage: " << argv[0] << " host port\n";
         exit(-1);
     }
-
+   
     char *host = argv[1];
     u_short port = atoi(argv[2]);
-    Thread_data thread_args[N_THREADS];
+    Commons c;
+    int count = c.COUNT;
+    Thread_data thread_args[count];
 
 	//Start Timer
 	Timer time;
@@ -120,7 +52,7 @@ int main(int argc, char *argv[])
 	time.start();
 
 	//Create Thread and do process
-    for (int i = 0; i < N_THREADS; ++i)
+    for (int i = 0; i < count; ++i)
     {
 
         Thread_data & t = thread_args[i];
@@ -131,7 +63,7 @@ int main(int argc, char *argv[])
     }
     
 	//Wait for each thread to finish
-    for (int i=0; i < N_THREADS; i++)
+    for (int i=0; i < count; i++)
         pthread_join(thread_args[i].thread_id, NULL);
 
 	//Stop timer and output elapsed time
