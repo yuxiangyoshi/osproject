02f5e30f57fbaea63e834685ac5c8ac7f0de347a
yuxiangyoshi <yuxiangyoshi@gmail.com>
2017-11-23 21:55:44 -0800

    threaded server

--- client.py
+++ client.py
@@ -1,25 +1,41 @@
 #client example
 import socket
+import threading
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 8888))
+def foo():
+    print("foo")
 
-# send 10 request for each range, then stop and close connection
-for i in range(10): 
-    client_socket.send("a".encode())
-    data = client_socket.recv(1024).decode()
-    print(data)
+def testconnect():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 8888))
+    print("inthread")
 
-for j in range(10):
-    client_socket.send("b".encode())
-    data = client_socket.recv(1024).decode()
-    print(data)
+    # send 10 request for each range, then stop and close connection
+    for i in range(10):
+        client_socket.send("cpu".encode())
+        data = client_socket.recv(1024).decode()
+        print(data)
 
-for k in range(10):
-    client_socket.send("c".encode())
-    data = client_socket.recv(1024).decode()
-    print(data)
+    for j in range(10):
+        # client_socket.connect(('localhost', 8888))
+        client_socket.send("balanced".encode())
+        data = client_socket.recv(1024).decode()
+        print(data)
 
-client_socket.send("bye".encode())
-client_socket.close()
+    for k in range(10):
+        # client_socket.connect(('localhost', 8888))
+        client_socket.send("io".encode())
+        data = client_socket.recv(1024).decode()
+        print(data)
+        
+    client_socket.send("end".encode())
+    client_socket.close()
 
+threads = []
+for i in range(5):
+    threads.append(threading.Thread(target=testconnect, args=()))
+
+for t in threads:
+    t.start()
+for t in threads:
+    t.join()
--- localserver.py
+++ localserver.py
@@ -1,58 +1,42 @@
 import socket
+import time
+import _thread
 from random import randint
 HOST, PORT = '', 8888
 
+def handle(client_connection):
+    while True:
+        request = client_connection.recv(1024).decode()
+        print(request)
+        if request == "cpu":
+            num = randint(190, 200)
+            latency = randint(0, 1)
+        elif request == "balanced":
+            num = randint(1800000, 1900000)
+            latency = randint(300, 500)
+        elif request == "io":
+            num = randint(19000000, 20000000)
+            latency = randint(1000, 2000)
+        elif request == "end":
+            client_connection.close()
+            break
+        else:
+            print("bad request")
+        time.sleep(float(latency)/1000)
+        http_response = str(num)
+        client_connection.sendall(http_response.encode())
+        # client_connection.sendall("Q".encode())
+        # client_connection.close()
+
+            
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
-listen_socket.listen(5)
+listen_socket.listen(10)
 print ('Serving HTTP on port %s ...' % PORT)
 
-a = [randint(0,50) for x in range(10)]
-b = [randint(100,500) for x in range(10)]
-c = [randint(1000,5000) for x in range(10)]
 
-print "Array a:"
-print(a)
-print "Array b:"
-print(b)
-print "Array c:"
-print(c)
-
-i = 0  # counter for selecting a
-j = 0
-k = 0
 while True:
     client_connection, client_address = listen_socket.accept()
-    if (i > 10):
-      i = 0
-    if (j > 10):
-      j = 0
-    if (k > 10):
-      k = 0
-    
-    while True:
-      request = client_connection.recv(1024).decode()
-      print(request)
-      if request == "a":
-        print(i)
-        astr = a[i%10]
-        http_response = str(astr)
-        client_connection.sendall(http_response.encode())
-        i += 1
-      elif request == "b":
-        print(j)
-        bstr = b[j%10]
-        http_response = str(bstr)
-        client_connection.sendall(http_response.encode())
-        j += 1
-      elif request == "c":
-        print(k)
-        cstr = c[k%10]
-        http_response = str(cstr)
-        client_connection.sendall(http_response.encode())
-        k += 1
-      else:
-        # client_connection.sendall("Q".encode())
-        client_connection.close()
-        break
+    print("before thread")
+    _thread.start_new_thread(handle, (client_connection,))
